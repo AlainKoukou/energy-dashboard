6daeca159be376dff65fd0b49900cfcc5571c022
AlainKoukou <alain.koukou@net.usj.edu.lb>
2026-04-24 13:04:58 +0300

    Updated from W to Kw measurements

--- server.js
+++ server.js
@@ -153,7 +153,7 @@ client.on("message", (topic, message) => {
         q_reactive: rawData.electrical_metrics?.reactive_power_var || 0,
         ai_distance: rawData. anomaly?.distance || 0,
         crest_factor: rawData.features?.crest_factor_current || 0,
-        deltaP: rawData.features?.delta_p_kw || 0
+        deltaP: rawData.features?.delta_p_watts || 0
       },
       anomalies: {
         voltage: rawData.anomaly?.severity === "critical", 
